f6f47d7770236097fbc40f849c56d77035d7b9fd
nurulamin092 <mnurulamin092@gmial.com>
2022-02-27 17:33:47 +0600

    card show display

--- js/app.js
+++ js/app.js
@@ -1,6 +1,29 @@
 const searchCards = () => {
-    const url = `https://deckofcardsapi.com/api/deck/new/draw/?count=10`;
+    const inputField = document.getElementById('input-field');
+    const inputValue = parseInt(inputField.value);
+    inputField.value = '';
+    const url = `https://deckofcardsapi.com/api/deck/new/draw/?count=${inputValue}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displaySearchCards(data.cards))
+}
+
+const displaySearchCards = cards => {
+    const cardList = document.getElementById('deck-cards');
+    cards.forEach(card => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+            <img src="${card.image}" class="card-img-top" alt="...">
+             <div class="card-body">
+                 <h5 class="card-title">${card.code}</h5>
+                 <h5 class="card-title">${card.suit}</h5>
+                 
+                   
+        </div>
+    `;
+        cardList.appendChild(div);
+    });
+    console.log(cards);
 }
